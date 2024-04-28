b1e9b047d18b2716753a0e58b4b6073b7e257cc8
Stanislav Musil <musil@sm-net.cz>
2024-04-28 20:59:28 +0200

    feat: changed from static dates to dynamic in timetable (from today till tomorrow)

--- get_timetable.js
+++ get_timetable.js
@@ -6,12 +6,18 @@ require('dotenv').config();
 
 const username=process.env.EDU_USERNAME;
 const password=process.env.EDU_PASSWORD;
+//Get tomorrow's date
+const tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate() + 1);
+//Get the day after tomorrow date
+const tomorrow2 = new Date();
+tomorrow2.setDate(tomorrow.getDate() + 2);
 
 (async () => {
     await edupage.login(username, password);
 
-    const from = new Date("2023-01-16");
-    const to = new Date("2023-01-17");
+    const from = tomorrow // new Date("2024-04-26"); // static date from which to get the timetable
+    const to = tomorrow2 //new Date("2024-04-27"); // static date to which to get the timetable
     const timetables = await edupage.fetchTimetablesForDates(from, to);
 
     console.log(timetables);
